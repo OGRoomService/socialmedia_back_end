3361f2069cad99d7319c8673859be189bd54edf3
Derek Ledford <Lehquack@gmail.com>
2021-09-23 16:01:15 -0400

    Editing config

--- src/main/java/com/mantaray/MainBackEnd.java
+++ src/main/java/com/mantaray/MainBackEnd.java
@@ -1,9 +1,14 @@
 package com.mantaray;
-
-public final class MainBackEnd 
+/**
+ * Generic Javadoc comment
+ */
+public final class MainBackEnd
 {
-    public static void main(String[] args) 
+    /**
+     * Generic Javadoc comment
+     */
+    public static void main(String[] args)
     {
         System.out.println("Hello World!");
-    }    
+    }
 }
