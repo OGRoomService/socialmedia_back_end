ba5eeaa102fd5eb7207159bcdffe2b06d85c03cb
Derek Ledford <Lehquack@gmail.com>
2021-09-23 19:22:42 -0400

    Added password verification, and unit tests for them to test every possible condition

---
+++ src/main/java/com/mantaray/PasswordVerification.java
@@ -0,0 +1,22 @@
+package com.mantaray;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PasswordVerification
+{
+    public boolean checkPassword(String password)
+    {
+        if(password.length() >= 8)
+        {
+            Matcher hasDigit = digit.matcher(password);
+            Matcher hasSymbol = symbol.matcher(password);
+
+            return (hasDigit.find() && hasSymbol.find());
+        }
+        return false;
+    }
+
+    private Pattern digit = Pattern.compile("[0-9]");
+    private Pattern symbol = Pattern.compile ("[!@#$%&*()_+=|<>?{}\\[\\]~-]");
+}
---
+++ src/test/java/com/mantaray/PasswordVerificationUnitTests/PasswordVerificationTest.java
@@ -0,0 +1,40 @@
+package com.mantaray.PasswordVerificationUnitTests;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import com.mantaray.PasswordVerification;
+
+public class PasswordVerificationTest
+{
+    PasswordVerification myPasswordVerifier = new PasswordVerification();
+
+    @Test
+    void givenAPasswordOfLengthLessThanEight_WhenCheckPasswordCalled_ThenReturnFalse() 
+    {
+        assertEquals(myPasswordVerifier.checkPassword("short"), false);
+    }
+
+    @Test
+    void givenAPasswordOfLengthEqualToEight_WhenCheckPasswordCalled_ThenReturnFalse() 
+    {
+        assertEquals(myPasswordVerifier.checkPassword("password"), false);
+    }
+
+    @Test
+    void givenAPasswordOfLengthEqualToEightWithNumbers_WhenCheckPasswordCalled_ThenReturnFalse() 
+    {
+        assertEquals(myPasswordVerifier.checkPassword("password1"), false);
+    }
+
+    @Test
+    void givenAPasswordOfLengthEqualToEightWithSymbols_WhenCheckPasswordCalled_ThenReturnFalse() 
+    {
+        assertEquals(myPasswordVerifier.checkPassword("password!"), false);
+    }
+
+    @Test
+    void givenAPasswordOfLengthEqualToEightWithSymbolsAndNumbers_WhenCheckPasswordCalled_ThenReturnTrue() 
+    {
+        assertEquals(myPasswordVerifier.checkPassword("password1!"), true);
+    }
+}
