d4830a3d5d20a7487b1918cd73cce67e3afb7c01
OGRoomService <51579862+OGRoomService@users.noreply.github.com>
2021-10-04 17:28:58 -0400

    commented out unused test

--- src/test/java/com/mantarays/socialbackend/SocialbackendApplicationTests.java
+++ src/test/java/com/mantarays/socialbackend/SocialbackendApplicationTests.java
@@ -1,13 +1,13 @@
 package com.mantarays.socialbackend;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SocialbackendApplicationTests {
 
-	@Test
+/* 	@Test
 	void contextLoads() {
-	}
+	} */
 
 }
