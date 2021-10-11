9a9d35f908a152eeb3a1382efb42a2140045ae79
Derek Ledford <Lehquack@gmail.com>
2021-10-11 00:02:19 -0400

    Fixed sharing preliminary stuff

--- src/main/java/com/mantarays/socialbackend/ServiceInterfaces/PostServiceIntf.java
+++ src/main/java/com/mantarays/socialbackend/ServiceInterfaces/PostServiceIntf.java
@@ -2,13 +2,14 @@ package com.mantarays.socialbackend.ServiceInterfaces;
 
 import com.mantarays.socialbackend.Models.Comment;
 import com.mantarays.socialbackend.Models.Post;
+import com.mantarays.socialbackend.Models.User;
 
 public interface PostServiceIntf 
 {
     Post createPost(Post post);
     Post getPost(Long post_id);
     Post savePost(Post post);
-    Post sharePost(Post post);
+    Post sharePost(Post post, User user);
 
     void updatePostText(Post post, String post_text);
 
