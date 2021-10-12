0e16135530781a6dec6833b4069b2a1c4897e629
Derek Ledford <Lehquack@gmail.com>
2021-10-11 20:07:00 -0400

    Token authorization added.

--- src/main/java/com/mantarays/socialbackend/Security/SecurityConfig.java
+++ src/main/java/com/mantarays/socialbackend/Security/SecurityConfig.java
@@ -1,12 +1,14 @@
 package com.mantarays.socialbackend.Security;
 
 import com.mantarays.socialbackend.Filter.CustomAuthenticationFilter;
+import com.mantarays.socialbackend.Filter.CustomAuthorizationFilter;
 import com.mantarays.socialbackend.Repositories.RoleRepository;
 import com.mantarays.socialbackend.Repositories.UserRepository;
 import com.mantarays.socialbackend.Services.UserService;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,6 +17,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import lombok.RequiredArgsConstructor;
 
@@ -38,13 +41,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
     @Override
     protected void configure(HttpSecurity http) throws Exception 
     {
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean(), new UserService(userRepo, roleRepo, passwordEncoder));
+
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        //http.authorizeRequests().antMatchers(HttpMethod.POST, "/login/**").permitAll();
-        //http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/user/**").hasAnyAuthority("ROLE_USER");
-        http.authorizeRequests().anyRequest().permitAll(); //TODO make this only allow ROLE_USER?
-        //http.authorizeRequests().anyRequest().authenticated();
-        http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean(), new UserService(userRepo, roleRepo, passwordEncoder)));
+        http.authorizeRequests().antMatchers(HttpMethod.POST, "/login/**").permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/users/create").permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/users/**").hasAnyAuthority("ROLE_USER");
+        http.authorizeRequests().anyRequest().authenticated();
+        http.addFilter(customAuthenticationFilter);
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
