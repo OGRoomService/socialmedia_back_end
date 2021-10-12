39a3e2fc2b6ebe134d8cc73031084f965d08def6
Derek Ledford <Lehquack@gmail.com>
2021-10-11 20:07:00 -0400

    Token authorization added.

---
+++ src/main/java/com/mantarays/socialbackend/Filter/CustomAuthorizationFilter.java
@@ -0,0 +1,87 @@
+package com.mantarays.socialbackend.Filter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.*;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CustomAuthorizationFilter extends OncePerRequestFilter
+{
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException 
+    {
+        if(request.getServletPath().equals("/api/login"))
+        {
+            filterChain.doFilter(request, response);
+        }
+        else
+        {
+            String authorizationHeader = request.getHeader("Authorization");
+            if(authorizationHeader != null && authorizationHeader.startsWith("Bearer "))
+            {
+                try
+                {
+                    String token = authorizationHeader.substring("Bearer ".length());
+                    Algorithm algorithm = Algorithm.HMAC256("TotallySecretLoginToken".getBytes());
+                    JWTVerifier verifier = JWT.require(algorithm).build();
+                    DecodedJWT decodedJWT = verifier.verify(token);
+                    String username = decodedJWT.getSubject();
+
+                    String[] roles = decodedJWT.getClaim("roles").asArray(String.class);
+                    Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+                    Stream<String> stream = Arrays.stream(roles);
+
+                    stream.forEach(role -> 
+                    {
+                        authorities.add(new SimpleGrantedAuthority(role));
+                    });
+
+                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, null, authorities);
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+                    filterChain.doFilter(request, response);
+                }
+                catch(Exception e)
+                {
+                    log.error("Error logging inL {} ", e.getMessage());
+                    response.setHeader("error", e.getMessage());
+                    response.setStatus(403);
+                    //response.sendError(403, e.getMessage());
+
+                    Map<String, String> error = new HashMap<>();
+                    error.put("error_message", e.getMessage());
+                    response.setContentType("application/json");
+                    new ObjectMapper().writeValue(response.getOutputStream(), error);
+                }
+            }
+            else
+            {
+                filterChain.doFilter(request, response);
+            }
+        }
+    }
+    
+}
