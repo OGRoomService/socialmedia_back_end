a63ec95933cb9b9303c5b41440fc93f66680e678
Derek Ledford <Lehquack@gmail.com>
2021-10-13 00:08:05 -0400

    Users can now create and like posts

--- src/main/java/com/mantarays/socialbackend/Security/SecurityConfig.java
+++ src/main/java/com/mantarays/socialbackend/Security/SecurityConfig.java
@@ -39,13 +39,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception 
+    protected void configure(HttpSecurity http) throws Exception
     {
         CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean(), new UserService(userRepo, roleRepo, passwordEncoder));
 
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeRequests().antMatchers(HttpMethod.POST, "/login/**").permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/posts/getUserPosts").permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/posts/createPost").permitAll(); //TODO remove
+        http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/posts/likePost").permitAll(); //TODO remove
+
         http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/users/create").permitAll();
         http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/users/**").hasAnyAuthority("ROLE_USER");
         http.authorizeRequests().anyRequest().authenticated();
