2ed7f572eb68a45e2ed41ec448e8dbcd41b45858
Derek Ledford <Lehquack@gmail.com>
2021-10-26 00:35:27 -0400

    Fixed a few things and cleaned it up. duplicate rows can no longer exist

---
+++ src/main/java/com/mantarays/socialbackend/VerificationServices/UserVerification.java
@@ -0,0 +1,57 @@
+package com.mantarays.socialbackend.VerificationServices;
+
+import com.mantarays.socialbackend.Forms.UserFailureStringsForm;
+import com.mantarays.socialbackend.Services.UserService;
+
+import java.util.Map;
+
+public class UserVerification
+{
+    private final UserService userService;
+    private final UsernameVerification usernameVerification;
+    private final PasswordVerification passwordVerification;
+    private final EmailVerification emailVerification;
+
+    public UserVerification(final UserService userService,
+                            final UsernameVerification usernameVerification,
+                            final PasswordVerification passwordVerification,
+                            final EmailVerification emailVerification)
+    {
+        this.userService = userService;
+        this.usernameVerification = usernameVerification;
+        this.passwordVerification = passwordVerification;
+        this.emailVerification = emailVerification;
+    }
+
+    public boolean doesAccountPassPreconditions(Map<String, String> inAccount, UserFailureStringsForm failureStrings)
+    {
+        boolean allPass = true;
+        if(userService.doesEmailExist(inAccount.get("email")))
+        {
+            failureStrings.emailFailureString = "Email already exists";
+            allPass = false;
+        }
+        if(userService.doesUsernameExist(inAccount.get("username")))
+        {
+            failureStrings.usernameFailureString = "Username already exists";
+            allPass = false;
+        }
+        if(!usernameVerification.checkUsername(inAccount.get("username")))
+        {
+            failureStrings.usernameFailureString = "Username failed preconditions";
+            allPass = false;
+        }
+        if(!passwordVerification.checkPassword(inAccount.get("password")))
+        {
+            failureStrings.passwordFailureString = "Password failed preconditions";
+            allPass = false;
+        }
+        if(!emailVerification.checkEmail(inAccount.get("email")))
+        {
+            failureStrings.emailFailureString = "Email failed preconditions";
+            allPass = false;
+        }
+
+        return allPass;
+    }
+}
